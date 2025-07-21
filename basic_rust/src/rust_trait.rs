13bee048e82f3b3e4f52fb7bd9c5d470f8db8f7e
CyberStackPro <yeabsragebriel@gmail.com>
2025-07-21 17:11:05 +0300

    Implement Draw trait and associated structures for dynamic rendering

--- basic_rust/src/rust_trait.rs
+++ basic_rust/src/rust_trait.rs
@@ -1,3 +1,54 @@
+/*
+dyn ->   means = It stores a vtable (a table of function pointers) at runtime to know which method to call.
+What is a Concrete Type in Rust? ->  A concrete type is a specific, fully-known type at compile time, like: i32, String, Vec<u8>
+Your own types: Tweet, NewsArticle, etc.
+So when we say "concrete type", we mean not a trait, and not an abstraction. It’s the actual struct or enum behind the scenes.
+The * is the dereference operator in Rust. It means:
+"Give me the value inside the reference."
+*/
+trait Draw {
+    fn draw(&self);
+}
+
+struct Button {
+    id: i8,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("Drawing a Button with ID: {}", self.id);
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+}
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!(
+            "Drawing a SelectBox of size {} X {}",
+            self.width, self.height
+        );
+    }
+}
+
+// A screen that can hold a collection of drawable objects
+struct Screen {
+    // `Box<dyn Draw>` means a Box containing *any* type that implements the `Draw` trait.
+    // The size of `Box<dyn Draw>` is known at compile time (it's just a pointer and vtable pointer).
+    // The actual type (Button or SelectBox) is only known at runtime.
+    components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
 pub trait Summary {
     fn summarize(&self) -> String;
 
@@ -10,6 +61,73 @@ pub trait Summary {
     }
 }
 
+enum Expression {
+    Literal(i32),
+    Add(Box<Expression>, Box<Expression>),
+    Subtract(Box<Expression>, Box<Expression>),
+    Multiply(Box<Expression>, Box<Expression>),
+}
+
+impl Expression {
+    /*
+       Multiply
+       ├── Add
+       │   ├── Literal(3) => 3
+       │   └── Literal(4) => 4
+       │   => 7
+       └── Subtract
+           ├── Literal(10) => 10
+           └── Literal(5) => 5
+           => 5
+       => Multiply(7, 5) => 35
+
+    */
+    fn evaluate(&self) -> i32 {
+        match &self {
+            Expression::Literal(val) => *val,
+            Expression::Add(left, right) => left.evaluate() + right.evaluate(),
+            Expression::Multiply(left, right) => left.evaluate() * right.evaluate(),
+            Expression::Subtract(left, right) => left.evaluate() - right.evaluate(),
+        }
+    }
+    fn to_string(&self) -> String {
+        match self {
+            Expression::Literal(val) => val.to_string(),
+            Expression::Add(left, right) => {
+                format!("({} + {})", left.to_string(), right.to_string())
+            }
+            Expression::Subtract(left, right) => {
+                format!("({} - {})", left.to_string(), right.to_string())
+            }
+            Expression::Multiply(left, right) => {
+                format!("({} * {})", left.to_string(), right.to_string())
+            }
+        }
+    }
+    // fn evaluate(&self) -> i32 {
+    //     match &self {
+    //         Expression::Literal(val) => *val,
+    //         Expression::Add(left, right) => {
+    //             let l = left.evaluate();
+    //             let r = right.evaluate();
+    //             println!("Add: {} + {} = {}", l, r, l + r);
+    //             l + r
+    //         }
+    //         Expression::Subtract(left, right) => {
+    //             let l = left.evaluate();
+    //             let r = right.evaluate();
+    //             println!("Subtract: {} - {} = {}", l, r, l - r);
+    //             l - r
+    //         }
+    //         Expression::Multiply(left, right) => {
+    //             let l = left.evaluate();
+    //             let r = right.evaluate();
+    //             println!("Multiply: {} * {} = {}", l, r, l * r);
+    //             l * r
+    //         }
+    //     }
+    // }
+}
 #[derive(Debug)]
 pub struct NewsArticle {
     pub headline: String,
@@ -47,21 +165,50 @@ impl Summary for Tweet {
 }
 
 fn main() {
-    let s = String::from("hi");
-    println!("{}", &s);
+    // let s = String::from("hi");
+    // println!("{}", &s);
 
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
+    // let tweet = Tweet {
+    //     username: String::from("horse_ebooks"),
+    //     content: String::from("of course, as you probably already know, people"),
+    //     reply: false,
+    //     retweet: false,
+    // };
+    // let article = NewsArticle {
+    //     headline: String::from("Penguins win the Stanley Cup!"),
+    //     location: String::from("Pittsburgh, PA"),
+    //     author: String::from("Iceburgh"),
+    //     content: String::from("The Pittsburgh Penguins once again won the Stanley Cup."),
+    // };
+
+    let screen = Screen {
+        components: vec![
+            Box::new(Button { id: 1 }),
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+            }),
+        ],
     };
-    let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup!"),
-        location: String::from("Pittsburgh, PA"),
-        author: String::from("Iceburgh"),
-        content: String::from("The Pittsburgh Penguins once again won the Stanley Cup."),
-    };
+
+    println!("\n--- Drawing Screen Components ---");
+    screen.run();
+
+    // Using Dynamic dispatch:  but this won't work b/c on our Summary trait we defined get_trait_version() is a static method ⇒ trait not object-safe ⇒ error
+    // let items: Vec<Box<dyn Summary>> = vec![
+    //     Box::new(Tweet {
+    //         username: String::from("horse_ebooks"),
+    //         content: String::from("hello world"),
+    //         reply: false,
+    //         retweet: false,
+    //     }),
+    //     Box::new(NewsArticle {
+    //         headline: String::from("Rust 2.0 Released!"),
+    //         location: String::from("Internet"),
+    //         author: String::from("Ferris"),
+    //         content: String::from("Exciting news..."),
+    //     }),
+    // ];
 
     // println!("Tweet summary: {}", tweet.summarize()); // Output: @horse_ebooks: of course, as you probably already know, people
     // println!("Article summary: {}", article.summarize()); // Output: Penguins win the Stanley Cup!, by Iceburgh (Pittsburgh, PA)
@@ -75,14 +222,14 @@ fn main() {
     //     <Tweet as Summary>::get_trait_version()
     // );
 
-    let largest_num = largest_i32(&[1, 20, 3, 4, 5, 6]);
+    // let largest_num = largest_i32(&[1, 20, 3, 4, 5, 6]);
 
-    let sentence = "hello are you fine yeabsra";
-    let chars: Vec<char> = sentence.chars().collect();
+    // let sentence = "hello are you fine yeabsra";
+    // let chars: Vec<char> = sentence.chars().collect();
 
-    let largest_char = largest_char(&chars);
+    // let largest_char = largest_char(&chars);
 
-    println!("Result {}", largest_char);
+    // println!("Result {}", largest_char);
 }
 
 // Generics
